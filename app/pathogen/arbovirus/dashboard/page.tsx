30b6d14166c20df87fecdd57d632dafec9205e66
HimanshuRanka <himanshuranka2612@gmail.com>
2023-11-26 15:07:17 -0500

    Updated the visualizations on the map page as well

--- app/pathogen/arbovirus/dashboard/page.tsx
+++ app/pathogen/arbovirus/dashboard/page.tsx
@@ -1,33 +1,31 @@
 import React from "react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
-import {
-  CountOfStudiesStratifiedByAntibodyAndPathogen,
-  CustomResponsiveBar,
-  PathogenSeroprevalenceBoxPlot,
-} from "@/app/pathogen/arbovirus/analyze/nivo-vis";
+import { Card, CardContent} from "@/components/ui/card";
 
 import MapAndFilters from "@/app/pathogen/arbovirus/dashboard/(map)/MapAndFilters";
+import {
+  AntibodyPathogenBar,
+  StudyCountOverTime,
+} from "../analyze/recharts";
 
 export default function ArbovirusDashboard() {
   // Need to make the visualizations dynamic. Unsure how to do this well using CSS.
   return (
     <>
-      <Card className={"row-span-1 col-span-4"}>
-        <CardHeader>
-          <CardTitle>Count Antibody Pathogen</CardTitle>
-        </CardHeader>
-        <CardContent className={"px-0 h-72 w-full"}>
-          <CountOfStudiesStratifiedByAntibodyAndPathogen />
+      <Card className={"row-span-1 col-span-4 px-4"}>
+        <CardContent className={"px-0 h-full flex flex-col"}>
+          <h3 className="py-4 w-full text-center text-lg">
+            Study count by pathogen & antibody type
+          </h3>
+          <AntibodyPathogenBar />
         </CardContent>
       </Card>
-      <Card className={"row-span-1 col-span-4"}>
-        <CardHeader>
-          <CardTitle>Patho Sero Boxplot</CardTitle>
-        </CardHeader>
-        <CardContent className={"px-0 h-72 w-full"}>
-          <PathogenSeroprevalenceBoxPlot />
+      <Card className={"row-span-1 col-span-4 px-4"}>
+        <CardContent className={"px-0 h-full flex flex-col"}>
+          <h3 className="py-4 w-full text-center text-lg">
+            Cumulative study count over time by pathogen
+          </h3>
+          <StudyCountOverTime />
         </CardContent>
       </Card>
       <MapAndFilters />
