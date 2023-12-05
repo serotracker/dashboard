039ef5a8efbecd7a8c6a09c9b076b09898d27a42
SeanKennyNF <sean.kenny@neofinancial.com>
2023-12-05 15:44:05 -0700

    fixed import on image
    Signed-off-by: SeanKennyNF <sean.kenny@neofinancial.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import HomepageMosquitoImage from "../public/HomepageMosquitoImage.jpg";
 import SarsCov2StructureImage from "../public/SarsCov2Structure.png";
-import SeroTrackerBackgroundImage from "../public/SeroTrackerBackgroundImage.png";
+import HomepageSeroTrackerBackgroundImage from "../public/HomepageSeroTrackerBackgroundImage.png";
 import { HomepageTile } from "./homepage-tile";
 
 export default function Home() {
