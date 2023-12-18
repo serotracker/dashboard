385960756286b4d121c8f5a5fddc22f9f91aa353
seank <seankenny6688@gmail.com>
2023-12-18 13:44:25 -0700

    Added div style to the card collection

--- components/ui/card-collection/card-collection.tsx
+++ components/ui/card-collection/card-collection.tsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 import { Card } from "../card";
-import { CardConfiguration, CardInputData, isFixedCardConfiguration } from "./card-collection-types";
+import {
+  CardConfiguration,
+  CardInputData,
+  isFixedCardConfiguration,
+} from "./card-collection-types";
 import { useCardCollectionConfiguration } from "./use-card-collection-configuration";
+
+enum CardStyle {
+  DIV = "DIV",
+  CARD = "CARD",
+}
 
 interface CardCollectionProps {
   cardInputData: CardInputData[];
+  cardStyle?: CardStyle;
   columnCountToFill: number;
 }
 
@@ -15,14 +25,19 @@ interface CardContentProps {
   cardConfigurations: CardConfiguration[];
 }
 
-const CardContent = ({cardWidthClass, cardConfiguration, cardConfigurations}: CardContentProps) => {
-  const [previousCardWidthClass, setPreviousCardWidthClass] = useState<string>(cardWidthClass);
+const CardContent = ({
+  cardWidthClass,
+  cardConfiguration,
+  cardConfigurations,
+}: CardContentProps) => {
+  const [previousCardWidthClass, setPreviousCardWidthClass] =
+    useState<string>(cardWidthClass);
 
   useEffect(() => {
-    if(cardWidthClass !== previousCardWidthClass) {
-      setPreviousCardWidthClass(cardWidthClass)
+    if (cardWidthClass !== previousCardWidthClass) {
+      setPreviousCardWidthClass(cardWidthClass);
 
-      if(isFixedCardConfiguration(cardConfiguration)) {
+      if (isFixedCardConfiguration(cardConfiguration)) {
         return;
       }
 
@@ -31,9 +46,13 @@ const CardContent = ({cardWidthClass, cardConfiguration, cardConfigurations}: Ca
   }, [previousCardWidthClass, cardWidthClass]);
 
   return cardConfiguration.renderCardContent({ cardConfigurations });
-}
+};
 
-export const CardCollection = ({ cardInputData, columnCountToFill }: CardCollectionProps) => {
+export const CardCollection = ({
+  cardInputData,
+  columnCountToFill,
+  cardStyle = CardStyle.CARD,
+}: CardCollectionProps) => {
   const { cardConfigurations } = useCardCollectionConfiguration({
     cardInputData,
     columnCountToFill,
@@ -41,20 +60,43 @@ export const CardCollection = ({ cardInputData, columnCountToFill }: CardCollect
 
   return (
     <>
-      {cardConfigurations.sort((a, b) => a.order > b.order ? 1 : -1).map((card) => {
-        const cardWidthClass = `span ${card.currentColumnCount} / span ${card.currentColumnCount}`;
+      {cardConfigurations
+        .sort((a, b) => (a.order > b.order ? 1 : -1))
+        .map((card) => {
+          const cardWidthClass = `span ${card.currentColumnCount} / span ${card.currentColumnCount}`;
 
-        return (
-          <Card
-            key={card.cardId}
-            className={card.cardClassname}
-            style={{gridColumn: cardWidthClass}}
-            hidden={card.currentColumnCount <= 0}
-          >
-            <CardContent cardWidthClass={cardWidthClass} cardConfiguration={card} cardConfigurations={cardConfigurations} />
-          </Card>
-        )
-      })}
+          if (cardStyle == CardStyle.CARD) {
+            return (
+              <Card
+                key={card.cardId}
+                className={card.cardClassname}
+                style={{ gridColumn: cardWidthClass }}
+                hidden={card.currentColumnCount <= 0}
+              >
+                <CardContent
+                  cardWidthClass={cardWidthClass}
+                  cardConfiguration={card}
+                  cardConfigurations={cardConfigurations}
+                />
+              </Card>
+            );
+          }
+
+          return (
+            <div
+              key={card.cardId}
+              className={card.cardClassname}
+              style={{ gridColumn: cardWidthClass }}
+              hidden={card.currentColumnCount <= 0}
+            >
+              <CardContent
+                cardWidthClass={cardWidthClass}
+                cardConfiguration={card}
+                cardConfigurations={cardConfigurations}
+              />
+            </div>
+          );
+        })}
     </>
   );
 };
