d16ef8856001f779ad1078a3f5438268b5df518d
seank <seankenny6688@gmail.com>
2023-12-30 11:08:20 -0700

    Fixed latitudes and longitudes on Serotracker map

--- app/pathogen/sarscov2/dashboard/(map)/MapAndFilters.tsx
+++ app/pathogen/sarscov2/dashboard/(map)/MapAndFilters.tsx
@@ -36,10 +36,8 @@ export default function MapAndFilters() {
                   cursor: "pointer",
                   dataPoints: state.filteredData.map((dataPoint) => ({
                     ...dataPoint,
-                    // Not a typo, there is some kind of underlying problem here worth investigating that
-                    // makes switching the latitude and longitude a required step to produce the correct results.
-                    longitude: dataPoint.pin_latitude,
-                    latitude: dataPoint.pin_longitude,
+                    longitude: dataPoint.longitude,
+                    latitude: dataPoint.pin_latitude,
                     // Convert this to some kind of unique identifier returned from the backend once the SARSCoV2 API returns unique ids.
                     id: `${dataPoint.pin_latitude}-${dataPoint.pin_longitude}-${dataPoint.studyName}`
                   })),
