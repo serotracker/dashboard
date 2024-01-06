2268f61c67efaa451b0425884b2ec146866cbfba
Gurick Kooner <kooner@ualberta.ca>
2024-01-06 15:53:40 -0700

    updated filter logic for overlaps

--- contexts/arbo-context.tsx
+++ contexts/arbo-context.tsx
@@ -75,20 +75,12 @@ function filterData(data: any[], filters: { [key: string]: string[] }): any[] {
         const itemStartDate = new Date(item.sample_start_date);
         let itemEndDate = new Date(item.sample_end_date);
 
-        console.log("START: ITEM START DATE: ", itemStartDate);
-        console.log("START: ITEM END DATE: ", itemEndDate);
-        console.log("START: FILTER START DATE: ", filterStartDate);
-
         // Check if the end date is before the start date (Fix for particular yellow fever studies in central africa that have start date 2009 and end date for 1969)
         if (itemEndDate < itemStartDate) {
           // Set the end date to be the same or 1 month after the start date
           itemEndDate = new Date(itemStartDate);
           itemEndDate.setMonth(itemEndDate.getMonth() + 1);
-
-          console.log("Adjusted ITEM END DATE: ", itemEndDate);
         }
-
-        console.log("STARTING CHECK: ", itemEndDate >= filterStartDate);
 
         // Check for any overlap in the sampling period
         return itemEndDate >= filterStartDate;
