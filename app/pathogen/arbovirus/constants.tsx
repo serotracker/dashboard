aead8933befac411d4a765428f002f462610ab74
EliasPinno <pucas946@gmail.com>
2024-01-12 16:11:22 -0800

    Created a constants file for common pathogen stuff.

---
+++ app/pathogen/arbovirus/constants.tsx
@@ -0,0 +1,29 @@
+interface PathogenInfo {
+    longForm: string; // long form name
+}
+
+const pathogenConstants: { [key: string]: PathogenInfo } = {
+    "DENV": { longForm: "Dengue Virus" },
+    "ZIKV": { longForm: "Zika Virus" },
+    "CHIKV": { longForm: "Chikungunya Virus"},
+    "YF": { longForm: "Yellow Fever"},
+    "WNV": { longForm: "West Nile Virus"},
+    "MAYV": { longForm: "Mayaro Virus"},
+};
+
+export type KnownPathogen = keyof typeof pathogenConstants extends infer Keys ? Keys extends string ? Keys : never : never;
+
+export const getAllKnownPathogens = (): string[] => {
+    return Object.keys(pathogenConstants);
+  };
+
+export const isKnownPathogen = (pathogen: string): pathogen is KnownPathogen => {
+    return pathogen in pathogenConstants;
+};
+
+export const getLongFormPathogen = (pathogen: KnownPathogen): string => {
+    if (!isKnownPathogen(pathogen)) {
+        throw new Error(`Attempted to get the longform of pathogen ${pathogen} which does not exist.`)
+    }
+    return pathogenConstants[pathogen].longForm
+}
