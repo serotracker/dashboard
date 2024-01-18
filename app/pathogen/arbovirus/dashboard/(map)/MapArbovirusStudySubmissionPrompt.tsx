90e10cd204a90b78a17a6766a24fd2e1ba69e314
seank <seankenny6688@gmail.com>
2024-01-17 18:46:32 -0700

    Changed the copy on the modal and made it open up the form in a new tab

--- app/pathogen/arbovirus/dashboard/(map)/MapArbovirusStudySubmissionPrompt.tsx
+++ app/pathogen/arbovirus/dashboard/(map)/MapArbovirusStudySubmissionPrompt.tsx
@@ -18,16 +18,15 @@ export const MapArbovirusStudySubmissionPrompt = ({
     <Card className={cn(className, hidden ? "hidden" : undefined)}>
       <CardHeader className={"p-2 flex flex-row space-y-0"}>
         <h3 className={"w-full text-center text-lg"}>
-          Don&apos;t see an arbovirus study listed?
+          Submit a source to us!
         </h3>
         <button className={"rounded-full hover:bg-gray-100 m-0 absolute right-2 top-2 p-1"} onClick={() => onClose()} aria-label="Close pop-up">
           <X />
         </button>
       </CardHeader>
       <CardContent className={"p-2 pt-0"}>
-        <p className="inline"> Please fill out the </p>
-        <Link className="inline text-link underline" target="_blank" href="https://forms.gle/pKNiMiMYr6hiKnXx8"> following form </Link>
-        <p className="inline"> so that we can add to our dataset. </p>
+        <p className="inline"> Are we missing an arbovirus seroprevalence study that we should include? Submit it to us via </p>
+        <Link className="inline text-link underline" target="_blank" href="https://forms.gle/pKNiMiMYr6hiKnXx8"> this form</Link>
       </CardContent>
     </Card>
   );
