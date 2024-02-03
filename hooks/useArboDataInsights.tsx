be73394967db2348a9482632bb47cd20d69b3db0
seank <seankenny6688@gmail.com>
2024-02-03 14:55:43 -0700

    Fixed names of types in useArboDataInsights

--- hooks/useArboDataInsights.tsx
+++ hooks/useArboDataInsights.tsx
@@ -1,13 +1,13 @@
 import { typedGroupBy } from "@/lib/utils";
 
-interface GetNumberOfUniqueValuesForField<TFieldName extends string> {
+interface GetNumberOfUniqueValuesForFieldInput<TFieldName extends string> {
   fieldName: TFieldName;
   filteredData: Record<TFieldName, string>[];
 }
 
 
 export const useArboDataInsights = () => {
-  const getNumberOfUniqueValuesForField = <TFieldName extends string>(input: GetNumberOfUniqueValuesForField<TFieldName>) => {
+  const getNumberOfUniqueValuesForField = <TFieldName extends string>(input: GetNumberOfUniqueValuesForFieldInput<TFieldName>) => {
     return Object.keys(typedGroupBy(input.filteredData, (dataPoint) => dataPoint[input.fieldName])).length;
   }
 
