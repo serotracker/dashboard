a564c990b9071981005979a1f94f5f759c4d190f
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-02-19 14:34:45 -0500

    Updated header style a little bit and also extracted a reusable segment of code

--- components/customs/header.tsx
+++ components/customs/header.tsx
@@ -28,7 +28,7 @@ type navMenuItem = {
   description: string;
 };
 
-const serotracker: navMenuItem[] = [
+const serotrackerNavItems: navMenuItem[] = [
   {
     title: "Dashboard",
     href: "/pathogen/sarscov2/dashboard",
@@ -48,7 +48,7 @@ const serotracker: navMenuItem[] = [
   },
 ];
 
-const arbotracker: navMenuItem[] = [
+const arbotrackerNavitems: navMenuItem[] = [
   {
     title: "Dashboard",
     href: `/pathogen/arbovirus/dashboard#${ArbovirusPageSectionId.MAP}`,
@@ -73,7 +73,7 @@ const arbotracker: navMenuItem[] = [
   },
 ];
 
-const about: navMenuItem[] = [
+const aboutNavItems: navMenuItem[] = [
   {
     title: "Our Team",
     href: "/pathogen/TODO",
@@ -93,17 +93,35 @@ const about: navMenuItem[] = [
   },
 ];
 
+interface TabGroupProps {
+  title: string;
+  navItems: navMenuItem[]
+}
+
+function TabGroup(props: TabGroupProps) {
+  return (
+    <div className="flex flex-col w-2/4 md:2/5 px-2">
+      <h2 className="mb-2">{props.title}</h2>
+      <ul className="flex flex-row space-x-2">
+        {props.navItems.map((navItem: navMenuItem) => (
+          <ListItem title={navItem.title} href={navItem.href}>
+            {navItem.description}
+          </ListItem>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
 export const Header = () => {
 
   const pathname = usePathname();
   const [titleSuffix, setTitleSuffix] = useState("Sero");
-  const [prevTitleSuffix, setPrevTitleSuffix] = useState("");
   const [titleSuffixColor, setTitleSuffixColor] = useState("text-background");
   const [headerBgColor, setHeaderBgColor] = useState("bg-background")
 
 
   useEffect(() => {
-    setPrevTitleSuffix(titleSuffix);
     if (pathname.includes('arbovirus')) {
       setTitleSuffix("Arbo");
       setTitleSuffixColor("text-green-500");
@@ -120,12 +138,12 @@ export const Header = () => {
   }, [pathname])
 
   return (
-    <header className={cn("flex items-center justify-between transition-colors duration-300 h-12 w-screen px-2 text-white border-b-4 border-zinc-100", headerBgColor)}>
-      <div className="cursor-pointer py-5 pl-2">
+    <header className={cn("flex items-center justify-between transition-colors duration-300 h-12 w-screen px-2 text-white border-b-4 border-white overflow-hidden", headerBgColor)}>
+      <div className="cursor-pointer pl-2">
         <Link href={"/"} className="flex items-center text-h1">
           <h1>
-          <span className={cn("p-1 mr-1 bg-white rounded-md transition-colors duration-300", titleSuffixColor)}>{titleSuffix}</span>
-          Tracker
+            <span className={cn("p-1 mr-1 bg-white transition-colors duration-300", titleSuffixColor)}>{titleSuffix}</span>
+            Tracker
           </h1>
         </Link>
       </div>
@@ -135,50 +153,23 @@ export const Header = () => {
             <NavigationMenuTrigger className="bg-transparent">Trackers</NavigationMenuTrigger>
             <NavigationMenuContent>
               <div className="p-4 flex justify-center w-full">
-                {!process.env.SARS_COV_2_TRACKER_ENABLED && 
-                <div className="flex flex-col w-2/4 px-2">
-                  <h2 className="mb-2">SC2Tracker</h2>
-                  <ul className="flex flex-row space-x-2">
-                  {serotracker.map((sc2Item: navMenuItem) => (
-                    <ListItem title={sc2Item.title} href={sc2Item.href}>
-                      {sc2Item.description}
-                    </ListItem>
-                  ))}
-                  </ul>
-                </div>
+                {process.env.SARS_COV_2_TRACKER_ENABLED &&
+                  <TabGroup title={"SC2Tracker"} navItems={serotrackerNavItems}                />
                 }
-                <div className="flex flex-col w-2/4 px-2">
-                  <h2 className="mb-2">ArboTracker</h2>
-                  <ul className="flex flex-row space-x-2">
-                  {arbotracker.map((arboItem: navMenuItem) => (
-                    <ListItem title={arboItem.title} href={arboItem.href}>
-                      {arboItem.description}
-                      </ListItem>
-                  ))}
-                  </ul>
-                </div>
-              </div>
+                <TabGroup title={"Arbotracker"} navItems={arbotrackerNavitems} />
+                              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger className="bg-transparent">About</NavigationMenuTrigger>
             <NavigationMenuContent>
-            <div className="p-4 flex justify-center w-full">
-                <div className="flex flex-col w-2/4 px-2">
-                  <h2 className="mb-2">About</h2>
-                  <ul className="flex flex-row space-x-2">
-                  {about.map((arboItem: navMenuItem) => (
-                    <ListItem title={arboItem.title} href={arboItem.href}>
-                      {arboItem.description}
-                      </ListItem>
-                  ))}
-                  </ul>
-                </div>
-              </div>
+              <div className="p-4 flex justify-center w-full">
+                <TabGroup title={"About"} navItems={aboutNavItems} />
+                              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
         </NavigationMenuList>
-        <NavigationMenuViewport className={cn("transition-colors duration-300", headerBgColor)}/>
+        <NavigationMenuViewport className={cn("transition-colors duration-300", headerBgColor)} />
       </NavigationMenu>
     </header>
   );
