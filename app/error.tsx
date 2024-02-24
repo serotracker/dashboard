fe0ebeb8bc036ba5c6bb0d4df2b23e8265e9e6a3
EliasPinno <pucas946@gmail.com>
2024-02-24 13:16:06 -0800

    Deleted useEffect import.

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,6 @@
 'use client' // Error components must be Client Components
  
 import { Button } from '@/components/ui/button'
-import { useEffect } from 'react'
  
 export default function Error({
   error,
