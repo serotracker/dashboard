40533a3bee176dee01e43c58228ad158c9a41acf
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-02-29 16:02:14 -0500

    Design 1 for hero section on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,151 @@
+import SeroMap from "../public/SeroMap.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMosquito,
+  faVirus,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
-import HomepageMosquitoImage from "../public/HomepageMosquitoImage.jpg";
-import SarsCov2StructureImage from "../public/SarsCov2Structure.png";
-import HomepageSeroTrackerBackgroundImage from "../public/HomepageSeroTrackerBackgroundImage.png";
-import { HomepageTile } from "./homepage-tile";
-import { ArbovirusPageSectionId } from "./constants";
+
+interface TrackerButtonProps {
+  titleSuffix: string;
+  description: string;
+  icon: IconDefinition;
+  textColor: string;
+  bgColor: string;
+}
+
+function TrackerButton(props: TrackerButtonProps) {
+  return (
+    <div
+      className={cn(
+        "w-full rounded-md text-white bg-white/90 flex flex-col mb-4 lg:mr-4 lg:last:mr-0 h-full overflow-hidden p-2 transition-all group hover:cursor-pointer"
+        , `hover:${props.bgColor}`
+      )}
+    >
+
+      <div className={cn("flex justify-between items-center w-full")}>
+        <h2 className={cn("p-2 w-full group-hover:text-white", props.textColor)}>
+          <span className={cn("p-1 text-white rounded-md mr-1 group-hover:bg-white", props.bgColor, `group-hover:${props.textColor}`)}>
+            {props.titleSuffix}
+          </span>
+          Tracker
+        </h2>
+        <FontAwesomeIcon
+          icon={props.icon}
+          width={36}
+          height={36}
+          className={cn("mr-4 group-hover:text-white", props.textColor)}
+        />
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between h-full bg-no-repeat"
-      style={{
-        backgroundImage: `url(${HomepageSeroTrackerBackgroundImage.src})`,
-        backgroundPosition: '40% 30%'
-      }}
-    >
-      <div>
-        <div className="flex flex-start mb-3 mt-[10%]">
-          <div>
-            <Image
-              src={"/DetailedSeroTrackerLogoWithText.png"}
-              width={377}
-              height={77}
-              alt={"SeroTracker"}
-              className="mb-3 mx-auto"
+    <div className="h-full overflow-auto">
+      <div
+        className="flex w-full h-full-screen bg-no-repeat p-4 lg:p-8"
+        style={{
+          backgroundImage: `url(${SeroMap.src})`,
+        }}
+      >
+        <div className="h-full w-full rounded-md  text-background p-8">
+          <div className="w-full bg-white/90 rounded-md p-4 mb-4">
+          <h1 className=" w-fit p-2 rounded-md">
+            SeroTracker
+          </h1>
+          <h3 className="rounded-md p-2">
+            Your Go to Source for COVID-19 and Arbovirus Seroprevalence Data
+          </h3>
+          <div className="p-2 rounded-md">
+            <p className=" w-fit mb-2">
+              We synthesize findings from thousands of COVID-19 and Arbovirus
+              seroprevalence studies worldwide, collect and standardize the data
+              we extract from them, provide useful analytics on the data and
+              extract monthly insights relating to tredns and patterns we find
+              in the data
+            </p>
+            <p className=" w-fit mb-2">
+              We conduct an ongoing systematic review to track serosurveys
+              (antibody testing-based surveillance efforts) around the world and
+              visualize findings on this dashboard.
+            </p>
+            <p>
+              Checkout our dashboards below!
+            </p>
+          </div>
+          </div>
+          
+          <h3 className="relative flex overflow-x-hidden text-background  bg-white/90 rounded-md mb-4 ">
+            <div className="animate-marquee whitespace-nowrap p-8">
+              <span>
+                We have data from <b>4,742,112</b> Participants accross
+              </span>
+              <span>
+                <b> 253,221 </b> Estimates from
+              </span>
+              <span>
+                <b> 94,213</b> Sources spanning
+              </span>
+              <span>
+                <b> 149</b> countries and territories
+              </span>
+            </div>
+            <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-8 px-4">
+              <span>
+                We have data for <b>4,742,112</b> Participants accross
+              </span>
+              <span>
+                <b> 253,221 </b> Estimates from
+              </span>
+              <span>
+                <b> 94,213</b> Sources spanning
+              </span>
+              <span>
+                <b> 149</b> countries and territories
+              </span>
+            </div>
+          </h3>
+          <div className="flex flex-col lg:flex-row">
+            {/*
+              These comments are for tailwindcss to pickup these classes so we do not need to add too many props
+              hover:bg-green-700
+              hover:text-green-700
+              group-hover:text-green-700
+            */}
+            <TrackerButton
+              titleSuffix="Arbo"
+              description="ArboTracker description"
+              icon={faMosquito}
+              bgColor="bg-green-700"
+              textColor="text-green-700"
             />
-            <h2>
-              A global dashboard standardizing pathogen and seroprevalence data
-            </h2>
+            {/*
+              hover:bg-blue-700
+              hover:text-blue-700
+              group-hover:text-blue-700
+            */}
+            <TrackerButton
+              titleSuffix="SC2"
+              description="SeroTracker description"
+              icon={faVirus}
+              bgColor="bg-blue-700"
+              textColor="text-blue-700"
+            />
           </div>
         </div>
       </div>
-      <div className={"flex w-full h-half-screen justify-around mb-4"}>
-        <HomepageTile
-          header="SARSCoV2Tracker"
-          subtitle="Access a collection of seroprevalence studies for SARS-CoV-2 that span across 38 million participants and 148 countries."
-          backgroundImage={SarsCov2StructureImage}
-          backgroundImageAttribution="By SPQR10 - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=88349537 / Cropped and modified to remove labels."
-          route="https://serotracker.com"
-        />
-        <HomepageTile
-          header="ArboTracker"
-          subtitle="View over a thousand seroprevalence estimates for arboviruses that span across over seventy countries and seven decades."
-          backgroundImage={HomepageMosquitoImage}
-          backgroundImageAttribution="By Own scan, slightly modified. Original by Emil August Goeldi (1859 - 1917). - E. A. Goeldi (1905) Os Mosquitos no Pará. Memorias do Museu Goeldi. Pará, Brazil. Figures 3 (left) 1 (middle) and 2 (right) of Plate 1 in the Appendix., Public Domain, https://commons.wikimedia.org/w/index.php?curid=5469706 / Modified to remove faded image of mosquito in top left corner and cropped."
-          route={`/pathogen/arbovirus/dashboard#${ArbovirusPageSectionId.MAP}`}
-        />
+      <div className="p-6 w-full bg-background">
+        <div className="bg-white rounded-md w-full flex justify-between px-8 lg:px-16 py-4 lg:py-6 items-center flex-wrap lg:flex-nowrap">
+        <Image src={"/WHO-EN-C-H.png"} alt={""} width={150} height={100}/>
+        <Image src={"/University-Of-Calgary-Logo.png"} alt={""} width={150} height={100}/>
+        <Image src={"/WHO-EN-C-H.png"} alt={""} width={150} height={100}/>
+        <Image src={"/University-Of-Calgary-Logo.png"} alt={""} width={150} height={100}/>
+        <Image src={"/WHO-EN-C-H.png"} alt={""} width={150} height={100}/>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -138,13 +138,23 @@ module.exports = {
           to: {
             height: 0
           }
-        }
+        },
+        "marquee": {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        "marquee2": {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "expand-downwards": "expand-downwards 0.4s forwards",
-        "collapse-upwards": "collapse-upwards 0.4s forwards"
+        "collapse-upwards": "collapse-upwards 0.4s forwards",
+        "marquee": 'marquee 25s linear infinite',
+        "marquee2": 'marquee2 25s linear infinite',
       },
       typography: {
         DEFAULT: {
