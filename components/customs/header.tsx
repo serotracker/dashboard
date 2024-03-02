307da4d4732d30f10f79c128b1d51ba68a5f27e2
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-03-02 14:25:53 -0500

    Addressed some comments about the copy, updated about page env var

--- components/customs/header.tsx
+++ components/customs/header.tsx
@@ -17,13 +17,13 @@ import { ArbovirusPageSectionId } from "@/app/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-type navMenuItem = {
+type NavMenuItem = {
   title: string;
   href: string;
   description: string;
 };
 
-const serotrackerNavItems: navMenuItem[] = [
+const serotrackerNavItems: NavMenuItem[] = [
   {
     title: "Dashboard",
     href: "/pathogen/sarscov2/dashboard",
@@ -43,37 +43,29 @@ const serotrackerNavItems: navMenuItem[] = [
   },
 ];
 
-const arbotrackerNavitems: navMenuItem[] = [
+const arbotrackerNavitems: NavMenuItem[] = [
   {
     title: "Dashboard",
     href: `/pathogen/arbovirus/dashboard#${ArbovirusPageSectionId.MAP}`,
-    description: "A dashboard for Arbovirus seroprevalence data",
+    description: "A dashboard for arbovirus seroprevalence data",
   },
   {
-    title: "Analysis",
+    title: "Data",
     href: `/pathogen/arbovirus/dashboard#${ArbovirusPageSectionId.TABLE}`,
-    description:
-      "A collection of visualizations and tabular data tools for our collection of arbovirus data",
+    description: "View or download our entire arbovirus dataset",
   },
   {
     title: "Visualizations",
     href: `/pathogen/arbovirus/dashboard#${ArbovirusPageSectionId.VISUALIZATIONS}`,
-    description:
-      "A collection of visualizations and tabular data tools for our collection of arbovirus data",
+    description: "A collection of visualizations for our arbovirus dataset",
   },
 ];
 
-const aboutNavItems: navMenuItem[] = [
-  {
-    title: "Our Team",
-    href: "/about/the-team",
-    description: "A list of our team members, alumni, stakeholders and other partners",
-  },
+const aboutNavItems: NavMenuItem[] = [
   {
     title: "Data Extraction",
     href: "/about/data-extraction",
-    description:
-      "The process used while extracted data",
+    description: "The process used to extract the data",
   },
   {
     title: "FAQ",
@@ -81,11 +73,17 @@ const aboutNavItems: navMenuItem[] = [
     description:
       "A list of frequently asked questions regarding the data, systematic review and organization",
   },
+  {
+    title: "Our Team",
+    href: "/about/the-team",
+    description:
+      "A list of our team members, alumni, stakeholders and other partners",
+  },
 ];
 
 interface TabGroupProps {
   title: string;
-  navItems: navMenuItem[]
+  navItems: NavMenuItem[];
 }
 
 function TabGroup(props: TabGroupProps) {
@@ -93,32 +91,34 @@ function TabGroup(props: TabGroupProps) {
     <div className="flex flex-col w-2/4 md:2/5 px-2">
       <h2 className="mb-2">{props.title}</h2>
       <ul className="flex flex-row space-x-2">
-        {props.navItems.map((navItem: navMenuItem) => (
-          <ListItem key={`${props.title}-${navItem.title}`} title={navItem.title} href={navItem.href}>
+        {props.navItems.map((navItem: NavMenuItem) => (
+          <ListItem
+            key={`${props.title}-${navItem.title}`}
+            title={navItem.title}
+            href={navItem.href}
+          >
             {navItem.description}
           </ListItem>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export const Header = () => {
-
   const pathname = usePathname();
   const [titleSuffix, setTitleSuffix] = useState("Sero");
   const [titleSuffixColor, setTitleSuffixColor] = useState("text-background");
-  const [headerBgColor, setHeaderBgColor] = useState("bg-background")
+  const [headerBgColor, setHeaderBgColor] = useState("bg-background");
 
-
-  // I wonder if there is a better way to do this without the useEffect. 
+  // I wonder if there is a better way to do this without the useEffect.
   // Will come back to it because I have spent too much time here already
   useEffect(() => {
-    if (pathname.includes('arbovirus')) {
+    if (pathname.includes("arbovirus")) {
       setTitleSuffix("Arbo");
       setTitleSuffixColor("text-arbovirus");
       setHeaderBgColor("bg-arbovirus delay-150");
-    } else if (pathname.includes('sarscov2')) {
+    } else if (pathname.includes("sarscov2")) {
       setTitleSuffix("SC2");
       setTitleSuffixColor("text-sc2virus");
       setHeaderBgColor("bg-sc2virus delay-150");
@@ -127,14 +127,26 @@ export const Header = () => {
       setTitleSuffixColor("text-background");
       setHeaderBgColor("bg-background");
     }
-  }, [pathname])
+  }, [pathname]);
 
   return (
-    <header className={cn("flex items-center  justify-between transition-colors duration-300 h-14 w-screen px-2 text-white border-b-4 border-white overflow-hidden", headerBgColor)}>
+    <header
+      className={cn(
+        "flex items-center  justify-between transition-colors duration-300 h-14 w-screen px-2 text-white border-b-4 border-white overflow-hidden",
+        headerBgColor
+      )}
+    >
       <div className="cursor-pointer pl-2">
         <Link href={"/"} className="flex items-center text-h1">
           <h2>
-            <span className={cn("p-1 mr-1 bg-white rounded-md transition-colors duration-300", titleSuffixColor)}>{titleSuffix}</span>
+            <span
+              className={cn(
+                "p-1 mr-1 bg-white rounded-md transition-colors duration-300",
+                titleSuffixColor
+              )}
+            >
+              {titleSuffix}
+            </span>
             Tracker
           </h2>
         </Link>
@@ -142,26 +154,38 @@ export const Header = () => {
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-transparent">Trackers</NavigationMenuTrigger>
+            <NavigationMenuTrigger className="bg-transparent">
+              Trackers
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <div className="p-4 flex justify-center w-full">
-                {process.env.SARS_COV_2_TRACKER_ENABLED &&
-                  <TabGroup title={"SC2Tracker"} navItems={serotrackerNavItems}                />
-                }
-                <TabGroup title={"Arbotracker"} navItems={arbotrackerNavitems} />
-                              </div>
+                {process.env.NEXT_PUBLIC_SARS_COV_2_TRACKER_ENABLED && (
+                  <TabGroup
+                    title={"SC2Tracker"}
+                    navItems={serotrackerNavItems}
+                  />
+                )}
+                <TabGroup
+                  title={"Arbotracker"}
+                  navItems={arbotrackerNavitems}
+                />
+              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-transparent">About</NavigationMenuTrigger>
+          {process.env.NEXT_PUBLIC_ABOUT_PAGE_ENABLED && <NavigationMenuItem>
+            <NavigationMenuTrigger className="bg-transparent">
+              About
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <div className="p-4 flex justify-center w-full">
                 <TabGroup title={"About"} navItems={aboutNavItems} />
-                              </div>
+              </div>
             </NavigationMenuContent>
-          </NavigationMenuItem>
+          </NavigationMenuItem>}
         </NavigationMenuList>
-        <NavigationMenuViewport className={cn("transition-colors duration-300", headerBgColor)} />
+        <NavigationMenuViewport
+          className={cn("transition-colors duration-300", headerBgColor)}
+        />
       </NavigationMenu>
     </header>
   );
