1c7450bf89dab2ce85821196daa1396efe91c4ac
EliasPinno <pucas946@gmail.com>
2024-03-01 18:44:38 -0800

    Corrected codegen file to be accurate.

--- codegen.ts
+++ codegen.ts
@@ -3,12 +3,12 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
    schema: 'https://iit-backend-v2.vercel.app/api/graphql',
    overwrite: true,
-   documents: ['src/api/*.tsx'],
+   documents: "./hooks/*.tsx",
    generates: {
       './gql/': {
         preset: 'client',
       },
-      "./gql/json/": {
+      "./gql/json/schema.json": {
         plugins: ["introspection"]
       }
    }
