8dd0a5fdbf0d9effd3366f6715dbce2f889f6df2
EliasPinno <pucas946@gmail.com>
2024-03-07 17:05:45 -0800

    Made loading page use shadcn component (still not working)

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,19 @@
 'use client'
-//import { Skeleton } from "@/components/ui/skeleton"
-import ClipLoader from "react-spinners/ClipLoader";
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
+    console.log("loading ran")
+    
     return (
-        <div className="h-full w-full flex items-center justify-center">
-            <ClipLoader color="#36d7b7" loading={true} speedMultiplier={1} size={'6em'}/>
+        <div className="h-full flex items-center justify-center space-y-3">
+            <div className="flex flex-col space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div>
         </div>
     )
 }
