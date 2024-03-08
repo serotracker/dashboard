91ebe60ffd94594b622e5b61ba56a5e8183642a5
EliasPinno <pucas946@gmail.com>
2024-03-07 19:36:21 -0800

    Completed loading page.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,19 +1,13 @@
-'use client'
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    console.log("loading ran")
     
     return (
-        <div className="h-full flex items-center justify-center space-y-3">
-            <div className="flex flex-col space-y-3">
-                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-                <div className="space-y-2">
-                    <Skeleton className="h-4 w-[250px]" />
-                    <Skeleton className="h-4 w-[200px]" />
-                </div>
-            </div>
+        <div className="h-full flex items-center justify-center gap-x-2 border-2 p-2">
+            <Skeleton className="align-middle h-full w-[15%]"/>
+            <Skeleton className="align-middle h-full w-[85%]"/>
         </div>
+
     )
 }
