0eb7c3af0cd14675c86f0a9918518120df28529b
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-03-08 15:53:40 -0500

    Added better number formatting

--- app/page.tsx
+++ app/page.tsx
@@ -175,7 +175,7 @@ export default async function Home() {
                 The designations employed and the presentation of the material
                 available on this platform does not imply the expression of any
                 opinion whatsoever on the part of WHO, SeroTracker, or
-                SeroTracker's partners concerning the legal status of any
+                SeroTracker&apos;s partners concerning the legal status of any
                 country, territory, city or area or of its authorities, or
                 concerning the delimitation of its frontiers or boundaries.
                 Dotted and dashed lines on maps represent approximate border
--- components/customs/arboStats.tsx
+++ components/customs/arboStats.tsx
@@ -2,12 +2,18 @@
 
 import { useArboDataStatistics } from "@/hooks/useArboStatistics";
 
+export function formatNumber(num?: number) {
+    return (
+      <b className="px-1">{num?.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') ?? 0}</b>
+      );
+}
+
 function ArboStats() {
     const arboDataStats = useArboDataStatistics();
 
     return (
         <h3 className="flex text-background bg-white rounded-md px-16 justify-center p-8 whitespace-nowrap">
-          {`We have data from ${arboDataStats.data?.arbovirusDataStatistics.patricipantCount ?? 0} Participants accross ${arboDataStats.data?.arbovirusDataStatistics.estimateCount ?? 0} Estimates from ${arboDataStats.data?.arbovirusDataStatistics.sourceCount ?? 0} Sources spanning ${arboDataStats.data?.arbovirusDataStatistics.countryCount ?? 0} countries and territories`}
+          We have data from {formatNumber(arboDataStats.data?.arbovirusDataStatistics.patricipantCount)} Participants accross {formatNumber(arboDataStats.data?.arbovirusDataStatistics.estimateCount)} Estimates from {formatNumber(arboDataStats.data?.arbovirusDataStatistics.sourceCount)} Sources spanning {formatNumber(arboDataStats.data?.arbovirusDataStatistics.countryCount)} countries and territories
       </h3>
     )
 }
