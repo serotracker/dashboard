dfd237f5956fee64a428a0d2970598e881d42e63
EliasPinno <pucas946@gmail.com>
2024-03-07 19:56:00 -0800

    Tinkering with padding.

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     
     return (
-        <div className="h-full flex items-center justify-center gap-x-2 border-2 p-2">
+        <div className="h-full flex items-center justify-center gap-x-2 border-2 py-4 px-4">
             <Skeleton className="align-middle h-full w-[15%]"/>
             <Skeleton className="align-middle h-full w-[85%]"/>
         </div>
