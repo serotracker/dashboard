bf4369968a9b6105006ae55f7f734cae4cb24bae
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-03-09 13:34:23 -0500

    Made the api key private

--- components/ui/pathogen-map/pathogen-map.tsx
+++ components/ui/pathogen-map/pathogen-map.tsx
@@ -142,7 +142,7 @@ export function PathogenMap<
       minZoom={2}
       maxZoom={14}
       interactiveLayerIds={[...layers.map((layer) => layer.id), 'country-highlight-layer']}
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY as string}
+      mapboxAccessToken={process.env.MAPBOX_API_KEY as string}
       onMouseEnter={onMouseEnter}
       onMouseDown={onMouseDown}
       onMouseLeave={onMouseLeave}
