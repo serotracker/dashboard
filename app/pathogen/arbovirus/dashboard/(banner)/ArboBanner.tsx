170a1ecb95dc1bd8599aad0c6652c2eb367ffb5f
EliasPinno <pucas946@gmail.com>
2024-03-31 08:44:37 -0700

    Deleted a console log.

--- app/pathogen/arbovirus/dashboard/(banner)/ArboBanner.tsx
+++ app/pathogen/arbovirus/dashboard/(banner)/ArboBanner.tsx
@@ -11,7 +11,6 @@ interface ArboBannerProps {
 
 export const ArboBanner = ({className}: ArboBannerProps) => {
     const state = useContext(ArboContext);
-
     const downloadData = () => {
         //console.log(state.filteredData)
         const csvConfig = mkConfig({
