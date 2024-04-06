8a818939a3f070269d374a3ba28e4676adbb9b5a
Himanshu Ranka <himanshuranka2612@gmail.com>
2024-04-06 14:12:47 -0400

    added todo

--- hooks/useIsLargeScreen.ts
+++ hooks/useIsLargeScreen.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect,  useMemo} from 'react';
 
 // Checks if the screen width is greater than 1024px (Need the useEffect to ensure it only happens on mount - useMemo broke the build)
+// TODO: see if there is a better way to do this
 export const useIsLargeScreen = () => {
   const [isLargeScreen, setIsLargeScreen] = useState(false);
 
