85821a97f1a12a8e9b5e79f9edefd6d8a0dda2f3
seank <seankenny6688@gmail.com>
2024-05-11 15:15:08 -0600

    Converted Estimate count by arbovirus and antibody type to use StackedBarChart, fixed label

---
+++ app/pathogen/arbovirus/dashboard/(visualizations)/estimate-count-by-arbovirus-and-antibody-type-graph.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { LegendConfiguration, StackedBarChart } from "@/components/customs/visualizations/stacked-bar-chart";
+import { ArboContext } from "@/contexts/pathogen-context/pathogen-contexts/arbo-context";
+import { arbovirusesSF, convertArboSFtoArbo } from "./recharts";
+import { sortArboviruses } from "./rechart-utils";
+import { generateRandomColour } from "@/lib/utils";
+
+interface EstimateCountByArbovirusAndAntibodyTypeGraphProps {
+  legendConfiguration: LegendConfiguration;
+}
+
+const barColoursForSampleFrames: Record<string, string | undefined> = {
+  "IgG": "#61f4de",
+  "IgM": "#65cbe9",
+  "IgG, IgM": "#6cb6ef",
+  "NAb": "#6c8dfa",
+};
+
+export const EstimateCountByArbovirusAndAntibodyTypeGraph = (props: EstimateCountByArbovirusAndAntibodyTypeGraphProps) => {
+  const state = useContext(ArboContext);
+
+  return (
+    <StackedBarChart
+      graphId='estimate-count-by-arbovirus-and-antibody-type-graph'
+      data={state.filteredData
+        .map((dataPoint) => ({...dataPoint, antibodyKey: dataPoint.antibodies.sort((antibodyA: string, antibodyB: string) => antibodyA > antibodyB ? 1 : -1).join(', ')}))
+        .filter((dataPoint) => !!dataPoint.antibodyKey)}
+      primaryGroupingFunction={(dataPoint) => convertArboSFtoArbo(dataPoint.pathogen as arbovirusesSF)}
+      primaryGroupingSortFunction={sortArboviruses}
+      secondaryGroupingFunction={(dataPoint) => dataPoint.antibodyKey}
+      secondaryGroupingSortFunction={(antibodyKeyA, antibodyKeyB) => antibodyKeyA > antibodyKeyB ? 1 : -1}
+      transformOutputValue={(data) => data.length}
+      legendConfiguration={props.legendConfiguration}
+      getBarColour={(antibodyKey) => barColoursForSampleFrames[antibodyKey] ?? generateRandomColour()}
+      xAxisTickSettings={{
+        idealMaximumCharactersPerLine: 5
+      }}
+    />
+  );
+}
