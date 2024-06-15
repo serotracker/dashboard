46be38851e13b7d7930a2dfd534e36e982b2becd
seank <seankenny6688@gmail.com>
2024-06-15 10:39:45 -0600

    Added information to the country popup

--- app/pathogen/mers/dashboard/(map)/mers-country-pop-up-content.tsx
+++ app/pathogen/mers/dashboard/(map)/mers-country-pop-up-content.tsx
@@ -1,5 +1,19 @@
-import { GenericMapPopUp, GenericMapPopUpWidth, HeaderConfigurationTextAlignment } from "@/components/ui/pathogen-map/map-pop-up/generic-map-pop-up";
-import { SarsCov2Estimate } from "@/contexts/pathogen-context/pathogen-contexts/sc2-context";
+import { useMemo } from 'react';
+import {
+  GenericMapPopUp,
+  GenericMapPopUpWidth,
+  HeaderConfigurationTextAlignment
+} from "@/components/ui/pathogen-map/map-pop-up/generic-map-pop-up";
+import {
+  AnimalMersEventMapMarkerData,
+  HumanMersEventMapMarkerData,
+  MersEstimateMapMarkerData,
+  MersMapMarkerData,
+  isMersEstimateMapMarkerData,
+  isMersFaoAnimalEventMapMarkerData,
+  isMersFaoHumanEventMapMarkerData
+} from "./shared-mers-map-pop-up-variables";
+import { PopUpContentRowType, PopupContentTextAlignment } from '@/components/ui/pathogen-map/map-pop-up/pop-up-content-rows';
 
 interface MersCountryPopupContentProps {
   record: {
@@ -8,11 +22,23 @@ interface MersCountryPopupContentProps {
     countryName: string,
     latitude: string,
     longitude: string,
-    dataPoints: Pick<SarsCov2Estimate, 'country'>[],
+    dataPoints: MersMapMarkerData[]
   }
 }
 
 export const MersCountryPopupContent = (props: MersCountryPopupContentProps): React.ReactNode => {
+  const { dataPoints } = props.record;
+
+  const allMersEstimates = useMemo(() => dataPoints.filter((dataPoint): dataPoint is MersEstimateMapMarkerData =>
+    isMersEstimateMapMarkerData(dataPoint))
+  , [ dataPoints ])
+  const allHumanMersEvents = useMemo(() => dataPoints.filter((dataPoint): dataPoint is HumanMersEventMapMarkerData =>
+    isMersFaoHumanEventMapMarkerData(dataPoint))
+  , [ dataPoints ])
+  const allAnimalMersEvents = useMemo(() => dataPoints.filter((dataPoint): dataPoint is AnimalMersEventMapMarkerData =>
+    isMersFaoAnimalEventMapMarkerData(dataPoint))
+  , [ dataPoints ])
+
   return (
     <GenericMapPopUp
       width={GenericMapPopUpWidth.THIN}
@@ -26,7 +52,32 @@ export const MersCountryPopupContent = (props: MersCountryPopupContentProps): Re
       topBannerConfiguration={{
         enabled: false
       }}
-      rows={[]}
+      rows={[
+        {
+          title: 'Seroprevalence Estimates'.replace(' ', '\u00A0'),
+          type: PopUpContentRowType.NUMBER as const,
+          value: allMersEstimates.length,
+          contentTextAlignment: PopupContentTextAlignment.RIGHT,
+          ribbonConfiguration: { ribbonColourClassname: 'bg-mers-estimate' },
+          rightPaddingEnabled: false
+        },
+        {
+          title: 'Confirmed Human Cases'.replace(' ', '\u00A0'),
+          type: PopUpContentRowType.NUMBER as const,
+          value: allHumanMersEvents.length,
+          contentTextAlignment: PopupContentTextAlignment.RIGHT,
+          ribbonConfiguration: { ribbonColourClassname: 'bg-mers-human-event' },
+          rightPaddingEnabled: false
+        },
+        {
+          title: 'Confirmed Animal Cases'.replace(' ', '\u00A0'),
+          type: PopUpContentRowType.NUMBER as const,
+          value: allAnimalMersEvents.length,
+          contentTextAlignment: PopupContentTextAlignment.RIGHT,
+          ribbonConfiguration: { ribbonColourClassname: 'bg-mers-animal-event' },
+          rightPaddingEnabled: false
+        },
+      ]}
       bottomBannerConfiguration={{
         enabled: false
       }}
