f0c30bf47085c9380ffe9774d9dae2020497bf32
seank <seankenny6688@gmail.com>
2024-06-28 10:49:36 -0600

    added back the country highlight layer

--- app/pathogen/arbovirus/dashboard/(map)/country-highlight-layers/esm-country-highlight-layer.tsx
+++ app/pathogen/arbovirus/dashboard/(map)/country-highlight-layers/esm-country-highlight-layer.tsx
@@ -13,15 +13,35 @@ export const useEsmCountryHighlightLayer = () => {
   >(input: GetCountryHighlightingLayerInformationInput<
     TData
   >): GetCountryHighlightingLayerInformationOutput => {
+    const allUniqueCountryCodesWithData = new Set(
+      input.data.map((dataPoint) => dataPoint.countryAlphaThreeCode)
+    );
+
+    const countryData = Array.from(
+      allUniqueCountryCodesWithData
+    ).map((countryAlphaThreeCode) => ({
+      countryAlphaThreeCode: countryAlphaThreeCode,
+      fill: MapSymbology.CountryFeature.HasData.Color,
+      opacity: MapSymbology.CountryFeature.HasData.Opacity,
+    }));
+
+    const countryHighlightLayerLegendEntries = [{
+      description: 'Seroprevalence estimates',
+      colour: MapSymbology.CountryFeature.HasData.Color
+    }, {
+      description: 'No seroprevalence estimates',
+      colour: MapSymbology.CountryFeature.Default.Color
+    }];
+
     return {
       paint: {
-        countryData: [],
+        countryData: countryData,
         defaults: {
           fill: MapSymbology.CountryFeature.Default.Color,
           opacity: MapSymbology.CountryFeature.Default.Opacity
         }
       },
-      countryHighlightLayerLegendEntries: []
+      countryHighlightLayerLegendEntries: countryHighlightLayerLegendEntries
     }
   }, []);
 
