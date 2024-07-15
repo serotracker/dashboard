b4fcde1c34b46990c75da8c700f1ba1baaf3cb56
seank <seankenny6688@gmail.com>
2024-07-15 11:53:54 -0600

    Improved the UX of the seroprev to cases/vaccinations viz

---
+++ components/ui/modal/customization-modal/multi-select-dropdown-customization-setting.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils';
+import { CustomizationSettingType } from "./customization-settings";
+import { Select, SelectProps } from '@/components/customs/select';
+
+export type MultiSelectDropdownCustomizationSettingProps = {
+  dropdownName: string;
+  type: CustomizationSettingType.MULTI_SELECT_DROPDOWN; 
+} & Omit<SelectProps, 'singleSelect'>;
+
+export const MultiSelectDropdownCustomizationSetting = (
+  props: Omit<MultiSelectDropdownCustomizationSettingProps, 'type'>
+) => {
+  const { className: _, ...propsWithoutClassname } = props;
+
+  return (
+    <div className={cn("w-full flex justify-between items-center", props.className ?? '')}>
+      <p> {props.dropdownName} </p>
+      <Select {...propsWithoutClassname} />
+    </div>
+  );
+}
