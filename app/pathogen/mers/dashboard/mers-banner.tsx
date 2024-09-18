c3a2ba850dc5d4159b3f981bde26afc9738b46f1
seank <seankenny6688@gmail.com>
2024-09-18 14:10:39 -0600

    better? copy

--- app/pathogen/mers/dashboard/mers-banner.tsx
+++ app/pathogen/mers/dashboard/mers-banner.tsx
@@ -78,7 +78,7 @@ export const MersBanner = () => {
     }}
     dataLastUpdatedNoteConfiguration={{
       enabled: true,
-      dataLastUpdatedText: 'The last major update to the data happened on September 14th 2024'
+      dataLastUpdatedText: 'The last major update to our data was on September 14th 2024'
     }}
   />
 }
