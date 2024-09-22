71b3a282592d42c785469fab0b6a396966ff2ccf
seank <seankenny6688@gmail.com>
2024-09-21 20:28:39 -0600

    edited MERS banner copy, changed visualization order

--- app/pathogen/mers/dashboard/mers-banner.tsx
+++ app/pathogen/mers/dashboard/mers-banner.tsx
@@ -54,9 +54,9 @@ export const MersBanner = () => {
   return <DashboardTopBanner
     headerContent={
       <div className="mb-2">
-        <p className="inline"> MERSTracker is a dashboard and platform for MERS serosurveys. We conduct an </p>
+        <p className="inline"> MERSTracker is a dashboard and platform for MERS serosurveys, viral testing, and genomic sequencing studies. We conduct an </p>
         <Link href="/about/about-our-data" className="underline text-link inline">ongoing systematic review</Link>
-        <p className="inline"> to track serosurveys (antibody testing-based surveillance efforts) around the world and visualize findings on this dashboard.</p>
+        <p className="inline"> to track surveillance efforts around the world and visualize findings on this dashboard.</p>
       </div>
     }
     downloadCsvButtonOneConfiguration={{
