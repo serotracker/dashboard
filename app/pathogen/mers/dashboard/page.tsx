dbec8e39fe88813ced593bead7906dd48f2a9efc
Sean Kenny <86806388+SeanKennyNF@users.noreply.github.com>
2024-09-24 16:04:21 -0600

    Added the WHO map disclaimer to MERSTracker (It's currently only in the landing page). (#547)
    Added the WHO map disclaimer to MERSTracker (It's currently only in the
    landing page).

--- app/pathogen/mers/dashboard/page.tsx
+++ app/pathogen/mers/dashboard/page.tsx
@@ -7,12 +7,14 @@ import { MersFilters } from "./filters";
 import { MersVisualizationsSection } from "./(visualizations)/visualizations-section";
 import { MersDataTable } from "./(table)/mers-data-table";
 import { MersBanner } from "./mers-banner";
+import { MersTrackerBetweenMapandDataSectionComponent } from "./merstracker-between-map-and-data-section-component";
 
 export default function SarsCov2Dashboard() {
   return (
     <GenericPathogenDashboardPage
       filtersComponent={MersFilters}
       mapSectionComponent={MersMap}
+      betweenMapAndDataSectionElement={MersTrackerBetweenMapandDataSectionComponent}
       dataSectionComponent={MersDataTable}
       banners={MersBanner}
       visualizationsSectionComponent={MersVisualizationsSection}
