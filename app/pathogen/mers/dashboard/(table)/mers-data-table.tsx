512d59646a5b43e2c7039e8f8b7f723915b785b5
seank <seankenny6688@gmail.com>
2024-11-25 23:14:01 -0700

    disable camel population data table

--- app/pathogen/mers/dashboard/(table)/mers-data-table.tsx
+++ app/pathogen/mers/dashboard/(table)/mers-data-table.tsx
@@ -78,7 +78,7 @@ export const MersDataTable = () => {
       ...(mersSeroprevalenceEstimateData.length > 0 ? [ AvailableMersDataTables.MERS_SEROPREVALENCE_ESTIMATES ] : []),
       ...(mersViralEstimateData.length > 0 ? [ AvailableMersDataTables.MERS_VIRAL_ESTIMATES ] : []),
       ...(((mersEventData.length > 0) && (process.env.NEXT_PUBLIC_FAO_EVENT_DATA_ENABLED === 'true')) ? [ AvailableMersDataTables.MERS_CASES ] : []),
-      ...(camelPopulationData.length > 0 ? [ AvailableMersDataTables.CAMEL_POPULATION_DATA ] : []),
+      //...(camelPopulationData.length > 0 ? [ AvailableMersDataTables.CAMEL_POPULATION_DATA ] : []),
       ...(estimatesWithGenomicSequencingData.length > 0 ? [ AvailableMersDataTables.GENOMIC_SEQUENCING_DATA ] : []),
       ...(process.env.NEXT_PUBLIC_WHO_MERS_CASES_DATA_ENABLED ? [ AvailableMersDataTables.WHO_MERS_CASES ] : [])
     ];
