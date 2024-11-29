6c323b7576d706c601cca19eb852e884d5a3550b
seank <seankenny6688@gmail.com>
2024-11-29 08:03:59 -0700

    fix map disclaimer

--- app/pathogen/mers/dashboard/merstracker-between-map-and-data-section-component.tsx
+++ app/pathogen/mers/dashboard/merstracker-between-map-and-data-section-component.tsx
@@ -1,7 +1,8 @@
 export const MersTrackerBetweenMapandDataSectionComponent = () => {
   return (
     <div className="w-full mt-4 mb-4 rounded-md bg-sky-100 p-4">
-      <p className="text-sm italic p-4"> The designations employed and the presentation of the material available on this platform does not imply the expression of any opinion whatsoever on the part of WHO, FAO, SeroTracker, or SeroTracker&apos;s partners concerning the legal status of any country, territory, city or area or of its authorities, or concerning the delimitation of its frontiers or boundaries. Dotted and dashed lines on maps represent approximate border lines for which there may not yet be full agreement. </p>
+      <p className="text-sm italic pb-2">The boundaries and names shown and the designations used on this map do not imply the expression of any opinion whatsoever on the part of WHO, FAO, SeroTracker, or SeroTracker&apos;s partners concerning the legal status of any country, territory, city or area or of its authorities, or concerning the delimitation of its frontiers or boundaries.</p>
+      <p className="text-sm italic">Note: Dotted lines represent approximately the Line of Control in Jammu and Kashmir agreed upon by India and Pakistan. The final status of Jammu and Kashmir has not yet been agreed upon by the parties. The final boundary between the Republic of South Sudan and the Republic of the Sudan has not yet been determined.</p>
     </div>
   )
 }
