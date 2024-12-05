e0200100594dbe2b822a88e23111d87e8bade2c9
Sean Kenny <86806388+SeanKennyNF@users.noreply.github.com>
2024-12-04 22:07:16 -0700

    Added a feedback form for MERSTracker. (#605)
    Added a feedback form for MERSTracker.

--- app/pathogen/mers/dashboard/(map)/mers-map-study-submission-prompt.tsx
+++ app/pathogen/mers/dashboard/(map)/mers-map-study-submission-prompt.tsx
@@ -18,7 +18,7 @@ export const MersMapStudySubmissionPrompt = ({
     <Card className={cn(className, hidden ? "hidden" : undefined)}>
       <CardHeader className={"p-2 pb-0 flex flex-row space-y-0"}>
         <p className={"w-full text-lg"}>
-          Submit a source to us!
+          Submit a source or feedback to us!
         </p>
         <button className={"rounded-full hover:bg-gray-100 m-0 absolute right-2 top-2 p-1"} onClick={() => onClose()} aria-label="Close pop-up">
           <X />
@@ -27,7 +27,9 @@ export const MersMapStudySubmissionPrompt = ({
       <CardContent className={"p-2"}>
         <p className="inline"> Are we missing a MERS seroprevalence or viral prevalence study that we should include? Submit it to us via </p>
         <Link className="inline text-link underline text-end" target="_blank" rel="noopener noreferrer" href="https://forms.gle/ifwicQVVjj9CeNoA9">this form</Link>
-        
+        <p className="inline">. Any feedback on the dashboard or feature suggestions can be directed to </p>
+        <Link className="inline text-link underline text-end" target="_blank" rel="noopener noreferrer" href="https://forms.gle/i2que2Rw3RRSQXum9">this form</Link>
+        <p className="inline">.</p>
       </CardContent>
     </Card>
   );
