f7ec27e7385b332ef99a76a3d0774329cb00f44f
Sean Kenny <seankenny6688@gmail.com>
2025-09-30 17:46:24 -0600

    cleanup
    Signed-off-by: Sean Kenny <seankenny6688@gmail.com>

--- components/ui/pathogen-map/disputed-border-layer.tsx
+++ components/ui/pathogen-map/disputed-border-layer.tsx
@@ -46,8 +46,6 @@ export const DisputedBorderLayer = (props: DisputedBorderLayerProps) => {
     return paint.countryData.find((dataPoint) => dataPoint.countryAlphaThreeCode === 'CHN')?.opacity;
   }, [ paint ]);
 
-  console.log('paint', paint);
-
   return (
     <>
       <Source
