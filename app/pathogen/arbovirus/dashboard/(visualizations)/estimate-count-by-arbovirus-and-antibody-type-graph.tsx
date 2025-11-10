cd8f80dd033c3cd0db7048d258635c7afe4de362
Sean Kenny <86806388+SeanKennyNF@users.noreply.github.com>
2025-11-10 14:58:12 -0700

    Tweaks to the estimate count by antibody type graph (#633)
    Tweaks to the estimate count by antibody type graph
    
    Signed-off-by: Sean Kenny <seankenny6688@gmail.com>

--- app/pathogen/arbovirus/dashboard/(visualizations)/estimate-count-by-arbovirus-and-antibody-type-graph.tsx
+++ app/pathogen/arbovirus/dashboard/(visualizations)/estimate-count-by-arbovirus-and-antibody-type-graph.tsx
@@ -11,13 +11,90 @@ interface EstimateCountByArbovirusAndAntibodyTypeGraphProps {
 }
 
 const barColoursForAntibodies: Record<string, string | undefined> = {
-  "IgG": "#61f4de",
-  "IgM": "#65cbe9",
-  "IgG, IgM": "#6cb6ef",
-  "NAb": "#6c8dfa",
-  "S segment (OROV only)": "#218dad",
-  "N Segment": "#09b1e3"
+  "NAb": "#61f4de",
+  "IgG": "#65cbe9",
+  "IgM": "#6cb6ef",
+  "IgG;IgM": "#6c8dfa",
+  "Total Ig": "#218dad",
+  "IgG;NAb": "#09b1e3",
+  "IgM;NAb": "#4b99f2",
+  "Other": "#2aa5eb",
 };
+
+const possibleAntibodiesForGraph = [
+  "NAb",
+  "IgG",
+  "IgM",
+  "IgG;IgM",
+  "Total Ig",
+  "IgG;NAb",
+  "IgM;NAb",
+  "Other",
+] as const;
+
+export type PossibleAntibodyForGraph = typeof possibleAntibodiesForGraph[number];
+
+const isPossibleAntibodyForGraph = (antibody: string): antibody is PossibleAntibodyForGraph => {
+  return possibleAntibodiesForGraph.some((element) => element === antibody);
+}
+
+const sortOrderForPossibleAntibodiesForGraph: Record<PossibleAntibodyForGraph, number> = {
+  "IgG": 1,
+  "IgG;IgM": 2,
+  "IgG;NAb": 3,
+  "IgM": 4,
+  "IgM;NAb": 5,
+  "NAb": 6,
+  "Other": 7,
+  "Total Ig": 8,
+}
+
+export const sortPossibleAntibodiesForGraph = (antibodyA: PossibleAntibodyForGraph, antibodyB: PossibleAntibodyForGraph): number => {
+  return sortOrderForPossibleAntibodiesForGraph[antibodyA] - sortOrderForPossibleAntibodiesForGraph[antibodyB];
+}
+
+interface GenerateAntibodyForGraphInput {
+  antibodies: string[];
+}
+
+interface GenerateAntibodyForGraphOutput {
+  antibody: PossibleAntibodyForGraph | undefined;
+}
+
+export const generateAntibodyForGraph = (
+  input: GenerateAntibodyForGraphInput
+): GenerateAntibodyForGraphOutput => {
+  const { antibodies } = input;
+
+  const cleanedAntibodies = antibodies
+    .map((antibody) => antibody.endsWith('-capture') && antibody.length > 8 ? antibody.slice(0, -8) : antibody)
+    .filter((antibody, index, array) => array.indexOf(antibody) === index)
+    .sort((antibodyA, antibodyB) => antibodyA > antibodyB ? 1 : -1);
+
+  if(cleanedAntibodies.length === 0) {
+    return {
+      antibody: undefined,
+    }
+  }
+
+  if(cleanedAntibodies.length > 2) {
+    return {
+      antibody: 'Other',
+    }
+  }
+
+  const antibody = cleanedAntibodies.join(';');
+
+  if(!isPossibleAntibodyForGraph(antibody)) {
+    return {
+      antibody: 'Other',
+    }
+  }
+
+  return {
+    antibody,
+  }
+}
 
 export const EstimateCountByArbovirusAndAntibodyTypeGraph = (props: EstimateCountByArbovirusAndAntibodyTypeGraphProps) => {
   const { primaryEstimateData: state } = useGroupedArbovirusEstimateData();
@@ -27,13 +104,18 @@ export const EstimateCountByArbovirusAndAntibodyTypeGraph = (props: EstimateCoun
     <StackedBarChart
       graphId='estimate-count-by-arbovirus-and-antibody-type-graph'
       data={state.filteredData
-        .map((dataPoint) => ({...dataPoint, antibodyKey: dataPoint.antibodies.sort((antibodyA: string, antibodyB: string) => antibodyA > antibodyB ? 1 : -1).join(', ')}))
-        .filter((dataPoint) => !!dataPoint.antibodyKey)
+        .map((dataPoint) => ({
+          ...dataPoint,
+          antibodyKey: generateAntibodyForGraph({ antibodies: dataPoint.antibodies }).antibody
+        }))
+        .filter((dataPoint): dataPoint is Omit<typeof dataPoint, 'antibodyKey'> & {
+          antibodyKey: NonNullable<typeof dataPoint['antibodyKey']>
+        } => !!dataPoint.antibodyKey)
       }
       primaryGroupingFunction={(dataPoint) => convertArboSFtoArbo(dataPoint.pathogen)}
       primaryGroupingSortFunction={sortArboviruses}
       secondaryGroupingFunction={(dataPoint) => dataPoint.antibodyKey}
-      secondaryGroupingSortFunction={(antibodyKeyA, antibodyKeyB) => antibodyKeyA > antibodyKeyB ? 1 : -1}
+      secondaryGroupingSortFunction={(antibodyKeyA, antibodyKeyB) => sortPossibleAntibodiesForGraph(antibodyKeyA, antibodyKeyB)}
       transformOutputValue={({ data }) => data.length}
       legendConfiguration={props.legendConfiguration}
       getBarColour={(antibodyKey) => barColoursForAntibodies[antibodyKey] ?? generateRandomColour()}
