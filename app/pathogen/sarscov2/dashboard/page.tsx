d3089291a44d150e9d71222fe690155342d87cce
Matt Taylor <73304688+cpcssn-mt@users.noreply.github.com>
2026-04-29 13:04:57 -0600

    Add SarsCov2 disclaimer (3).

--- app/pathogen/sarscov2/dashboard/page.tsx
+++ app/pathogen/sarscov2/dashboard/page.tsx
@@ -31,12 +31,12 @@ function SarsCov2Banner() {
       <div className="flex justify-start items-center gap-6">
         <Link
           href="#TABLE"
-          class="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-[30%] bg-background hover:bg-backgroundHover h-full">
+          className="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-[30%] bg-background hover:bg-backgroundHover h-full">
           Download CSV with Seroprevalence Estimates
         </Link>
         <Link
           href="/about/about-our-data"
-          class="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-[30%] bg-background hover:bg-backgroundHover ml-2 h-full">
+          className="flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-[30%] bg-background hover:bg-backgroundHover ml-2 h-full">
           Cite our Data
         </Link>
       </div>
